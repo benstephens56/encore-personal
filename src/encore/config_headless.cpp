81887c0faf3cb30df05a535d10e54ccd26b12cb1
benstephens56 <49702729+benstephens56@users.noreply.github.com>
2026-02-23 12:40:19 -0500

    Add texture dumping and custom texture settings

--- src/encore/config_headless.cpp
+++ src/encore/config_headless.cpp
@@ -219,6 +219,8 @@ void Config_Headless::LoadNonSyncSettings() {
     ReadSetting(Settings::values.resolution_factor);
     ReadSetting(Settings::values.texture_filter);
     ReadSetting(Settings::values.texture_sampling);
+    ReadSetting(Settings::values.dump_textures);
+    ReadSettings(Settings::values.custom_textures);
 
     ReadSetting(Settings::values.mono_render_option);
     ReadSetting(Settings::values.render_3d);
